258ed37d325f3fa52722dfcdbdb87d9ec4e7724e
Armin Khosroabadi <arminkhosroabadi1387@gmail.com>
2024-01-06 14:10:59 +0330

    Update: Pulse

--- src/components/Pulse/Pulse.jsx
+++ src/components/Pulse/Pulse.jsx
@@ -2,42 +2,58 @@ import React from "react";
 
 export function Pulse({ color = "black", size = "64px" }) {
   const css = `.aniload-pulse {
-        width: ${size};
-        height: ${size};
-        display: inline-block;
-        position: relative;
-      }
-      .aniload-pulse::after,
-      .aniload-pulse::before {
-        content: no-open-quote;  
-        box-sizing: border-box;
-        width: ${size};
-        height: ${size};
-        border-radius: 50%;
-        background: ${color};
-        position: absolute;
-        left: 0;
-        top: 0;
-        animation: aniload-pulse 2s linear infinite;
-      }
-      .aniload-pulse::after {
-        animation-delay: 1s;
-      }
-      
-      @keyframes aniload-pulse {
-        0% {
-          transform: scale(0);
-          opacity: 1;
-        }
-        100% {
-          transform: scale(1);
-          opacity: 0;
-        }
-      }
+    display: inline-block;
+    position: relative;
+    width: ${size};
+    height: ${size};
+  }
+  .aniload-pulse div {
+    position: absolute;
+    border: 4px solid ${color};
+    opacity: 1;
+    border-radius: 50%;
+    animation: aniload-pulse 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+  .aniload-pulse div:nth-child(2) {
+    animation-delay: -0.5s;
+  }
+  @keyframes aniload-pulse {
+    0% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 0;
+    }
+    4.9% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 0;
+    }
+    5% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 1;
+    }
+    100% {
+      top: 0px;
+      left: 0px;
+      width: 72px;
+      height: 72px;
+      opacity: 0;
+    }
+  }
+  
           `;
   return (
-    <div className="aniload-pulse">
+    <div class="aniload-pulse">
       <style>{css}</style>
+      <div></div>
+      <div></div>
     </div>
   );
 }
